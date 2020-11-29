dca7b28c2fc282064e8b33e084becb3362bdc7e6
Parv Dave <parvdave29@gmail.com>
2020-11-29 13:03:28 +0530

    Added Member and Event tables

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    firstName = models.CharField(max_length=200,blank=True)
+    lastName = models.CharField(max_length=200,blank=True)
+    stream = models.CharField(max_length=200,blank=True)
+    course = models.CharField(max_length=200,blank=True)
+    year = models.CharField(max_length=200,blank=True)
+    position = models.CharField(max_length=200,blank=True)
+    deptName = models.CharField(max_length=200,blank=True)
+    img = models.ImageField(upload_to='corepics',blank=True)
+    quote = models.TextField(max_length=400,blank=True)
+    def __str__(self):
+        return self.firstName+ ' ' + self.lastName + f' ({self.deptName})'
--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    eventName = models.CharField(max_length=200,blank=True)
+    deptName = models.CharField(max_length=200,blank=True)
+    desc = models.TextField(max_length=200,blank=True)
+    rules = models.TextField(max_length=200,blank=True)
+    def __str__(self):
+        return self.eventName+f" ({self.deptName})"
